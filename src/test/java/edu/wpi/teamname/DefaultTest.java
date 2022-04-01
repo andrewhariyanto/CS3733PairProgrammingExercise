e9148473b668360217e125c377be6678ffd3480d
Patrick Hunter <pehunter@wpi.edu>
2022-04-01 15:26:41 -0400

    Added test cases

--- src/test/java/edu/wpi/teamname/DefaultTest.java
+++ src/test/java/edu/wpi/teamname/DefaultTest.java
@@ -11,5 +11,26 @@ import org.junit.Test;
 public class DefaultTest {
 
   @Test
-  public void test() {}
+  public void testRectanglePerimeter() {
+    Rectangle rect = new Rectangle(10, 20);
+    assert (rect.getPerimeter() == 60.0);
+  }
+
+  @Test
+  public void testRectanglePerimeterZero() {
+    Rectangle rect = new Rectangle(10, 0);
+    assert (rect.getPerimeter() == 20.0);
+  }
+
+  @Test
+  public void testRectangleArea() {
+    Rectangle rect = new Rectangle(10, 20);
+    assert (rect.getPerimeter() == 200.0);
+  }
+
+  @Test
+  public void testRectangleAreaZero() {
+    Rectangle rect = new Rectangle(10, 0);
+    assert (rect.getPerimeter() == 0.0);
+  }
 }
