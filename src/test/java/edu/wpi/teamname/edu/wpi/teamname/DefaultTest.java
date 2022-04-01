c85dc5cf88cb6440d15c316a6e78b77d4a30e7ec
ash-cmd <andrew.hariyanto@gmail.com>
2022-04-01 15:38:02 -0400

    Added circle tests

--- src/test/java/edu/wpi/teamname/edu/wpi/teamname/DefaultTest.java
+++ src/test/java/edu/wpi/teamname/edu/wpi/teamname/DefaultTest.java
@@ -5,6 +5,7 @@
 package edu.wpi.teamname;
 
 import org.junit.Test;
+import org.junit.Assert;
 
 // import org.junit.jupiter.api.Test; (JUnit 5 equivalent)
 
@@ -13,24 +14,49 @@ public class DefaultTest {
   @Test
   public void testRectanglePerimeter() {
     Rectangle rect = new Rectangle(10, 20);
-    assert (rect.getPerimeter() == 60.0);
+    Assert.assertTrue(rect.getPerimeter() == 60.0);
   }
 
   @Test
   public void testRectanglePerimeterZero() {
     Rectangle rect = new Rectangle(10, 0);
-    assert (rect.getPerimeter() == 20.0);
+    Assert.assertTrue(rect.getPerimeter() == 20.0);
   }
 
   @Test
   public void testRectangleArea() {
     Rectangle rect = new Rectangle(10, 20);
-    assert (rect.getArea() == 200.0);
+    Assert.assertTrue(rect.getArea() == 200.0);
   }
 
   @Test
   public void testRectangleAreaZero() {
     Rectangle rect = new Rectangle(10, 0);
-    assert (rect.getArea() == 0.0);
+    Assert.assertTrue(rect.getArea() == 0.0);
   }
+
+  @Test
+  public void testCirclePerimeterZero(){
+    Circle circ = new Circle(0);
+    Assert.assertTrue(circ.getPerimeter() == 0);
+  }
+
+  @Test
+  public void testCirclePerimeter(){
+    Circle circ = new Circle(3);
+    Assert.assertTrue(circ.getPerimeter() == 2*3*Math.PI);
+  }
+
+  @Test
+  public void testCircleAreaZero(){
+    Circle circ = new Circle(0);
+    Assert.assertTrue(circ.getArea() == 0);
+  }
+
+  @Test
+  public void testCircleArea(){
+    Circle circ = new Circle(3);
+    Assert.assertTrue(circ.getArea() == 9*Math.PI);
+  }
+
 }
